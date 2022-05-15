e2ea0a8d70a45a8586680623128488cc330956e8
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-15 12:53:57 +0500

    Task 2.2

--- Task_2.2/Program.cs
+++ Task_2.2/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Write_Files(string[] s)
+    {
+        string[] Pathes = new string[s.Length];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            using (StreamWriter writetext = new StreamWriter($"File.{i}.txt"))
+            {
+                writetext.WriteLine(s[i]);
+                Pathes[i] = $"File.{i}.txt";
+            }
+        }
+
+        Reverse_Files(s, Pathes);
+    }
+
+    static void Reverse_Files(string[] s, string[] Pathes)
+    {
+        string[] Path_to_Files = new string[s.Length];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            char[] cArray = s[i].ToCharArray();
+            string[] reverse = new string[s.Length];
+
+            for (int j = cArray.Length - 1; j > -1; j--)
+            {
+                if (cArray[j] >= 'A' && cArray[j] < 'Z')
+                {
+                    reverse[i] += cArray[j-j];
+                }
+                else if (cArray[j] >= 'a' && cArray[j] < 'z' || cArray[j] == ' ')
+                {
+                    reverse[i] += cArray[j];
+                }
+            }
+
+            using (StreamWriter writetext = new StreamWriter($"Changed_File.{i}.txt"))
+            {
+                Path_to_Files[i] = $"Changed_File.{i}.txt";
+                writetext.WriteLine(reverse[i]);
+                File.Delete(Pathes[i]);
+            }
+        }
+
+        Read_Files(Path_to_Files);
+    }
+
+    static void Read_Files(string[] files)
+    {
+        foreach (var item in files)
+        {
+            using (StreamReader readtext = new StreamReader($"{item}"))
+            {
+                Console.WriteLine(readtext.ReadLine());
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mockfiles = new string[3];
+        for (int i = 0; i < 3; i++)
+        {
+            mockfiles[i] = Console.ReadLine();
+        }
+        Write_Files(mockfiles);
+    }
+}
