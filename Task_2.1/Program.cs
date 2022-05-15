b6d44f1c3e4b95c2e4929d24f8d4b04e3725ac9e
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-15 12:31:09 +0500

    Task 2.1

---
+++ Task_2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+double res = Math.Log(b);
+
+int fact = 1;
+for (int x = 1; x <= a; x++)
+{
+    fact *= x;
+}
+Console.WriteLine(
+    (fact > res ? 
+        $"Number 'a' = {fact * 0.05:0.00} and more than number 'b'= {res:0.00}"
+        : $"Number 'a' = {fact * 0.05:0.00} and less than number 'b' = {res:0.00}"));
